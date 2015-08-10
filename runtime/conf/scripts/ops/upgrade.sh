2bdff577f2667922f3321bd321792eccb3e108a2
der <dave@epimorphics.com>
2015-08-10 21:08:49 +0100

    Fix use of incorrect AWS key

--- runtime/conf/scripts/ops/upgrade.sh
+++ runtime/conf/scripts/ops/upgrade.sh
@@ -11,16 +11,16 @@ readonly IP=$( jq -r .address "$SERVER/config.json" )
 . /opt/dms/conf/scripts/config.sh
 
 # Complete update
-ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP sudo apt-get -yq update
-ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP sudo apt-get -yq upgrade
-ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP sudo apt-get -yq dist-upgrade
+ssh -t -t $SSH_FLAGS -i $AWS_KEY -l ubuntu $IP sudo apt-get -yq update
+ssh -t -t $SSH_FLAGS -i $AWS_KEY -l ubuntu $IP sudo apt-get -yq upgrade
+ssh -t -t $SSH_FLAGS -i $AWS_KEY -l ubuntu $IP sudo apt-get -yq dist-upgrade
 
 # Force a reboot to install any dist upgrades
-ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP sudo reboot
+ssh -t -t $SSH_FLAGS -i $AWS_KEY -l ubuntu $IP sudo reboot
 
 # Wait for machine to come up again
 sleep 10s
-ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP echo "Server up"
+ssh -t -t $SSH_FLAGS -i $AWS_KEY -l ubuntu $IP echo "Server up"
 
 # Good luck pause to allow services to start as well
 sleep 5s
