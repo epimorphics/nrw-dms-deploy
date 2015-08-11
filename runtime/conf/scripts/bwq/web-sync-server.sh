a10de0f4ad91f2e39b7c6fcee84d73f0252c4e7f
der <dave@epimorphics.com>
2015-08-11 09:41:02 +0100

    Add web sync to presentation server startup/activate

---
+++ runtime/conf/scripts/bwq/web-sync-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Web sync operation for single server
+# Arguments:
+#     serverDir
+
+set -o errexit
+[[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
+. ./config.sh
+
+readonly serverDir="$1"
+
+echo "Synchronizing web content, including source/dump files"
+cd $serverDir/../../../../Web
+FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem"
+IP=$( jq -r .address "$serverDir/config.json" )
+rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/environment/html
