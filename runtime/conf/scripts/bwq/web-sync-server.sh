6f34ce7167aa2820825af7de613588e5f88bdc98
der <dave@epimorphics.com>
2015-09-01 11:32:10 +0100

    Fix sync target for web-sync

--- runtime/conf/scripts/bwq/web-sync-server.sh
+++ runtime/conf/scripts/bwq/web-sync-server.sh
@@ -13,4 +13,5 @@ echo "Synchronizing web content, including source/dump files"
 cd $serverDir/../../../../Web
 FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/nrw.pem"
 IP=$( jq -r .address "$serverDir/config.json" )
-rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/environment/html
+rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/environment-nrw/html/wales/bathing-waters
+
