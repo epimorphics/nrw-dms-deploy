4c72ebddf4cbc6e500d287f11ceca1e47f2108b7
der <dave@epimorphics.com>
2016-06-09 11:58:46 +0100

    Alternative approach to Rhyl East kludge

--- runtime/conf/scripts/bwq/forecast-rhyl.sh
+++ runtime/conf/scripts/bwq/forecast-rhyl.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 # Kludge forecast upload to duplicate Rhyl to Rhyl East (vomitsome)
-# Usage:  forecast-rhyl  uploadfile uploadURI
+# Usage:  forecast-rhyl  uploadfile 
 
-# [[ $# = 2 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
+[[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
 readonly file="$1"
-readonly orig="${file}.orig"
-readonly updateURI=$2
 
 if grep -sq 40600 $file && grep -sqv 40650 $file ; then
-    cp $file $orig
-    echo "" >> $file  # Fix lack of line ending for last line
-    grep 40600 $orig | sed -e 's/40600/40650/' >> $file
-    echo "Duplicated Rhyl forecast to Rhyl East"
-    if [[ $updateURI =~ .*/([^/]*)$ ]]; then
-        uid=${BASH_REMATCH[1]}
-        echo "Attemping to reconvert modified file"
-        curl -i -u forecastuser:r1SuN4MsNf -H  -X POST http://localhost/dms/services/nrwbwq/components/forecasts/update/$uid/reconvert
-        sleep 5s
+    if [[ $file =~ (.*)\.csv ]]; then
+        refile="${BASH_REMATCH[1]}-rhyl-east.csv"
+    else
+        refile="${file}-rhyl-east.csv"
     fi
+    head -1 $file > $refile
+    grep 40600 $file | sed -e 's/40600/40650/' >> $refile
+    echo "Attempting to publish secondary forecast for Rhyl East"
+    curl -s -u forecastuser:r1SuN4MsNf -H "Content-Type: text/csv" -X POST --data-binary "@$refile" http://localhost:8080/dms/api/nrwbwq/components/forecasts/publishTesting
 else
     echo "Either no Rhyl forecast or already a Rhyl East, skipping"
 fi
