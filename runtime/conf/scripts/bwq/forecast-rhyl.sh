18f04a0088696e2118c504f8bfc9633a2d33fa73
der <dave@epimorphics.com>
2016-06-09 09:23:28 +0100

    Experimental fudge for Rhyl East

---
+++ runtime/conf/scripts/bwq/forecast-rhyl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Kludge forecast upload to duplicate Rhyl to Rhyl East (vomitsome)
+# Usage:  forecast-rhyl  upload
+
+[[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
+readonly file="$1"
+readonly orig="${file}.orig"
+
+if grep -sq 40600 $file && grep -sqv 40650 $file ; then
+    cp $file $orig
+    echo "" >> $file  # Fix lack of line ending for last line
+    grep 40600 $orig | sed -e 's/40600/40650/' >> $file
+    echo "Duplicated Rhyl forecast to Rhyl East"
+else
+    echo "Either no Rhyl forecast or already a Rhyl East, skipping"
+fi
