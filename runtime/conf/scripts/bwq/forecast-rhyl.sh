5454d137f649463bddaab9526af56c3702b4748e
der <dave@epimorphics.com>
2016-06-10 08:45:21 +0100

    Finish switching Rhyl East kludge to production

--- runtime/conf/scripts/bwq/forecast-rhyl.sh
+++ runtime/conf/scripts/bwq/forecast-rhyl.sh
@@ -14,7 +14,7 @@ if grep -sq 40600 $file && grep -sqv 40650 $file ; then
     head -1 $file > $refile
     grep 40600 $file | sed -e 's/40600/40650/' >> $refile
     echo "Attempting to publish secondary forecast for Rhyl East"
-    curl -s -u forecastuser:r1SuN4MsNf -H "Content-Type: text/csv" -X POST --data-binary "@$refile" http://localhost:8080/dms/api/nrwbwq/components/forecasts/publishTesting
+    curl -s -u forecastuser:r1SuN4MsNf -H "Content-Type: text/csv" -X POST --data-binary "@$refile" http://localhost:8080/dms/api/nrwbwq/components/forecasts/publishProduction
 else
     echo "Either no Rhyl forecast or already a Rhyl East, skipping"
 fi
