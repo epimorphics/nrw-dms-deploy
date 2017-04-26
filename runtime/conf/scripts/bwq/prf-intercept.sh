79b419eb0eef0eb0ea7b2566890c729bdefb4638
der <dave.reynolds@epimorphics.com>
2017-04-26 12:00:19 +0100

    Remove old and suspect version of Rhyl intercept

--- runtime/conf/scripts/bwq/prf-intercept.sh
+++ runtime/conf/scripts/bwq/prf-intercept.sh
@@ -17,16 +17,6 @@ sed -i -e '/39950/d' -e '/39700/d' $file
 if grep -sq 40600 $file && grep -sqv 40650 $file ; then
     echo "Duplicating Rhyl forcecast to Rhyl East"
     grep 40600 $file | sed -e 's/40600/40650/' >> $file
-
-#    if [[ $file =~ (.*)\.csv ]]; then
-#        refile="${BASH_REMATCH[1]}-rhyl-east.csv"
-#    else
-#        refile="${file}-rhyl-east.csv"
-#    fi
-#    head -1 $file > $refile
-#    grep 40600 $file | sed -e 's/40600/40650/' >> $refile
-#    echo "Attempting to publish secondary forecast for Rhyl East"
-#    curl -s -u forecastuser:r1SuN4MsNf -H "Content-Type: text/csv" -X POST --data-binary "@$refile" http://localhost:8080/dms/api/nrwbwq/components/forecasts/publishProduction
 else
     echo "Either no Rhyl forecast or already a Rhyl East, skipping"
 fi
