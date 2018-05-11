9870e82b3b1df08f634423eb2151eff188567271
der <dave.reynolds@epimorphics.com>
2018-05-11 15:11:50 +0100

    Add forwarding for Cemaes PRFs

--- runtime/conf/scripts/bwq/prf-intercept.sh
+++ runtime/conf/scripts/bwq/prf-intercept.sh
@@ -14,6 +14,15 @@ if grep -qi prediction_text_en $file ; then
   # Ensure file ends in newline, the PRFs as supplied don't
   sed -i -e '$a\' $file
 
+  # Special case for Cemaes to forward before deletion
+  if grep -sq 40050 $file ; then
+    echo "Forwarding forecast for Cemaes"
+    CEMAES_PRF=$( grep 40050 $file | sed -e 's/,0,/,Unknown risk,/' -e 's/,1,/,Normal risk,/' -e 's/,2,/,Increased risk,/' -e 's/,/, /g' )
+    CEMAES_SNS_TOPIC=arn:aws:sns:eu-west-1:828737851284:cemaes-prf
+#    CEMAES_SNS_TOPIC=arn:aws:sns:eu-west-1:853478862498:dave_epimorphics_com
+    aws sns publish --topic-arn $CEMAES_SNS_TOPIC --subject "Cemaes forecast" --message "EA Forecast for Cemaes: $CEMAES_PRF"
+  fi
+
   echo "Removing forecasts for Aberdaron and Criccieth"
   sed -i -e '/39950/d' -e '/39700/d' $file
 
