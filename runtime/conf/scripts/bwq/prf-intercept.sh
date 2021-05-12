594d1ab1c887741f5ffa631c6fcbe5852e9bb799
der <dave@epimorphics.com>
2021-05-12 11:14:40 +0100

    Update PRF intercept for 2021 changes
    - duplicating Rhyl forecast to Abergele and Kinmel Bay as well
    - drop forwarding of incoming Cemaes forecast

--- runtime/conf/scripts/bwq/prf-intercept.sh
+++ runtime/conf/scripts/bwq/prf-intercept.sh
@@ -15,19 +15,21 @@ if grep -qi prediction_text_en $file ; then
   sed -i -e '$a\' $file
 
   # Special case for Cemaes to forward before deletion
-  if grep -sq 40050 $file ; then
-    echo "Forwarding forecast for Cemaes"
-    CEMAES_PRF=$( grep 40050 $file | sed -e 's/,0,/,Unknown risk,/' -e 's/,1,/,Normal risk,/' -e 's/,2,/,Increased risk,/' -e 's/,/, /g' )
-    CEMAES_SNS_TOPIC=arn:aws:sns:eu-west-1:828737851284:cemaes-prf
-    aws sns publish --topic-arn $CEMAES_SNS_TOPIC --region=eu-west-1 --subject "Cemaes forecast" --message "EA Forecast for Cemaes: $CEMAES_PRF"
-  fi
+#  if grep -sq 40050 $file ; then
+#    echo "Forwarding forecast for Cemaes"
+#    CEMAES_PRF=$( grep 40050 $file | sed -e 's/,0,/,Unknown risk,/' -e 's/,1,/,Normal risk,/' -e 's/,2,/,Increased risk,/' -e 's/,/, /g' )
+#    CEMAES_SNS_TOPIC=arn:aws:sns:eu-west-1:828737851284:cemaes-prf
+#    aws sns publish --topic-arn $CEMAES_SNS_TOPIC --region=eu-west-1 --subject "Cemaes forecast" --message "EA Forecast for Cemaes: $CEMAES_PRF"
+#  fi
 
   echo "Removing forecasts for Aberdaron, Criccieth and Cemaes"
   sed -i -e '/39950/d' -e '/39700/d' -e '/40050/d' $file
 
   if grep -sq 40600 $file && grep -sqv 40650 $file ; then
-    echo "Duplicating Rhyl forcecast to Rhyl East"
+    echo "Duplicating Rhyl forcecast to Rhyl East, Abergele and Kinmel Bay"
     grep 40600 $file | sed -e 's/40600/40650/' >> $file
+    grep 40600 $file | sed -e 's/40600/40450/' >> $file
+    grep 40600 $file | sed -e 's/40600/40500/' >> $file
   else
     echo "Either no Rhyl forecast or already a Rhyl East, skipping"
   fi
